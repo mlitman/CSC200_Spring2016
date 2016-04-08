f1ca986a6d5d3964384227ce179d3552433a02e2
Mike Litman <mike@awesomefat.com>
2016-04-08 11:59:01 -0500

    introduced indexOf

--- Driver.java
+++ Driver.java
@@ -5,8 +5,45 @@ public class Driver
 	{
 		String s = "hello";
 		String s2 = "elephant";
-		Driver.displayString(s);
-		Driver.displayWithSpaces(s);
+	}
+	
+	//returns a new String with all of the vowels
+	//removed form the parameter String
+	//"hello" -> hll
+	static String removeVowels(String s)
+	{
+		return "";
+	}
+	
+	//returns a new String with all of the consonants
+	//removed form the parameter String
+	//"hello" -> eo
+	static String removeConsonants(String s)
+	{
+		return "";
+	}
+		
+	//returns a new String with all of the numbers
+	//removed form the parameter String
+	//"he11o" -> heo
+	static String removeDigits(String s)
+	{
+		return "";
+	}
+		
+	//return the first occurence in s where c is found or -1 if 
+	//it was not found - we can't use the String's indexOf
+	static int indexOf(String s, char c)
+	{
+		for(int i = 0; i < s.length(); i = i + 1)
+		{
+			if(s.charAt(i) == c)
+			{
+				return i;
+			}
+		}
+		//if we are still kicking....
+		return -1;
 	}
 	
 	static void displayString(String s)
@@ -30,8 +67,13 @@ public class Driver
 	
 	//this method should display on a single line the input String
 	//in reverse: "hello" -> "olleh"
-	static void displayInReverse(String s)
+	static String reverseString(String s)
 	{
-		
+		String answer = "";
+		for(int i = s.length()-1; i >= 0; i = i - 1)
+		{
+			answer = answer + s.charAt(i);
+		}
+		return answer;
 	}
 }
